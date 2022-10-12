05c1116e42e88d4671e0ca28e3dd68eb69d52b37
lducoding <lducoding@gmail.com>
2022-10-12 17:03:40 +0900

    employee 생성자 위치 수정

--- src/main/java/chapter07/payment4/Employee.java
+++ src/main/java/chapter07/payment4/Employee.java
@@ -2,14 +2,13 @@ package chapter07.payment4;
 
 public abstract class Employee {
 
+    protected String name;
+    protected int basePay;
+
     public Employee(String name, int basePay) {
         this.name = name;
         this.basePay = basePay;
     }
-
-    protected String name;
-    protected int basePay;
-
     public abstract double calculatePay(double taxRate);
     public abstract int monthlyBasePay();
 
@@ -17,7 +16,4 @@ public abstract class Employee {
         return name;
     }
 
-    public int getBasePay() {
-        return basePay;
-    }
 }
