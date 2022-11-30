27cedfa33312d2b4de7f2286f98d62ebac6be54b
lducoding <lducoding@gmail.com>
2022-11-30 19:33:02 +0900

    프로그래밍언어 에서의 타입

--- src/main/java/chapter13/Start.java
+++ src/main/java/chapter13/Start.java
@@ -1,9 +1,16 @@
 package chapter13;
 
 public class Start {
+    private static int a = 1;
+    private static int b = 2;
+    private static String c ="1";
+    private static String d ="2";
+
     public static void main(String[] args) {
-        Square square = new Square(10,10,10);
-        resize(square, 50, 100);
+//        Square square = new Square(10,10,10);
+//        resize(square, 50, 100);
+        System.out.println(a+b);
+        System.out.println(c+d);
     }
 
     public static void resize(Rectangle rectangle, int width, int height) {
