68f3a426d1410494bbe264d0fff14f5aca77db93
lducoding <lducoding@gmail.com>
2022-12-07 09:53:29 +0900

    test

--- src/main/java/chapter13/Start.java
+++ src/main/java/chapter13/Start.java
@@ -6,7 +6,7 @@ class Bird {
     };
 }
 
-class Penguin2 extends Bird{
+class Penguin2 {
     public Penguin2() {
         System.out.println("펭귄 ");
     }
@@ -14,7 +14,11 @@ class Penguin2 extends Bird{
 
 public class Start {
     public static void main(String[] args) {
-        Bird penguin = new Penguin2();
-        penguin.fly();
+//        Bird penguin = new Penguin2();
+//        penguin.fly();
+        UnFlying penguin = new Penguin();
+        penguin.egg();
+//        UnFlyingBird pepe = new Peng();
+//        pepe.egg();
     }
 }
